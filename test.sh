f77a78903b91036a9ab4578d066d44151d2143e8
Shradha <shradhakamat711@gmail.com>
2026-08-02 13:05:09 +0000

    Added test shell script

---
+++ test.sh
@@ -0,0 +1,15 @@
+###### use of | (pipe in grep command)
+#
+#let's say if you want to print only 1 from all the numbers.
+#
+echo 1
+echo 11
+echo 19
+echo 100
+echo 56
+echo 78
+
+
+
+####### execute script /.test.sh | grep 1 so it only prints the numbers which has 1 in it.
+
