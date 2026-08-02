5c7ab7eb3b1afbe3aceefb7b9987b32c4c460ebc
Shradha <shradhakamat711@gmail.com>
2026-08-02 11:24:54 +0000

    Added node health monitoring shell script

---
+++ nodeHealth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+##################
+#
+# Author: Shradha
+# Date: 02/08/2026
+#
+# This script outputs the node Health
+#
+# Version: V1
+#
+##############################
+#
+#
+set -x #default mode
+df -h
+
+
+free -g
+
+
+nproc
+
+
